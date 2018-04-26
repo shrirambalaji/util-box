80722107130afeb91a7da8e215f6cfa66c7dd001
Shriram Balaji <>
2018-04-27 02:01:49 +0530

    Fixing queryString params bug by normalising req params, added more unit tests

--- test/unit/http.util.test.js
+++ test/unit/http.util.test.js
@@ -2,6 +2,31 @@ const path = require("path");
 const test = require("ava");
 const HOMEDIR = path.join(__dirname, "..", "..");
 const httpUtil = require(path.join(HOMEDIR, "http.util"));
+const { mockRes } = require("../fixtures/http.util.fixtures");
+test("removes trailing slash from a url, if present", t => {
+  t.plan(1);
+  let responseUrl = httpUtil.removeTrailingSlash("test-check.com/");
+  t.is(responseUrl, "test-check.com");
+});
+
+test("normalises and removes undefined request parameters", t => {
+  t.plan(1);
+  let mockReqParams = {
+    name: "Jon Snow",
+    death: undefined,
+    title: "The King in the North",
+    knows: undefined // knows nothing
+  };
+  let responseParams = httpUtil.normalizeReqParams(mockReqParams);
+  t.deepEqual(
+    {
+      name: "Jon Snow",
+      title: "The King in the North"
+    },
+    responseParams
+  );
+});
+
 test("returns a querystring when provided with request params", t => {
   t.plan(1);
   let mockReqParams = {
@@ -14,4 +39,25 @@ test("returns a querystring when provided with request params", t => {
     queryString,
     "?firstName=Walter&lastName=White&quote=I'm the one who knocks!"
   );
+});
+
+test("handles generic api response, and returns a promise", async t => {
+  try {
+    const apiResponse = await httpUtil.handleApiResponse(mockRes);
+    t.deepEqual({ message: "Hello" }, apiResponse);
+  } catch (error) {
+    t.falsy(error);
+  }
+});
+
+test("returns an error, for a non-2xx response", async t => {
+  t.plan(3);
+  mockRes.status = 400;
+  try {
+    const failingApiResponse = await httpUtil.handleApiResponse(mockRes);
+  } catch (err) {
+    t.truthy(err);
+    t.is(`Expected 2xx, found ${mockRes.status}`, err.message);
+    t.is(err.statusCode, mockRes.status);
+  }
 });
